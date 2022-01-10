b4ea1ae03f15d884d41172d0130d3fb0e5bc587f
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-11 00:56:46 +0300

    add material design ui

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-
-  </div>
+  <v-navigation-drawer v-model="drawer" color="white" app mini-variant mini-variant-width="80">
+      <v-avatar class="d-block text-center mx-auto mt-4">
+        <v-icon color="#49D9A0" large>mdi-atlassian</v-icon>
+      </v-avatar>
+  </v-navigation-drawer>
 </template>
 
 <script>
-
 export default {
   name: 'SideBar',
   components: {},
@@ -14,9 +15,28 @@ export default {
       selectItem: 0,
       drawer: null,
       items: [
-        {icon:'fa'}
+        {icon:'plus'},
+        {icon:'th-large'},
+        {icon:'align-center'},
+        {icon:'gitter'},
+        {icon:'chart-line'},
       ]
     }
   }
 }
 </script>
+
+<style>
+.border  {
+  margin-left: 12px;
+  margin-right: 12px;
+  background: #49D9A0 ;
+  border-radius: 50%;
+  text-decoration: none;
+
+}
+.v-list-item-group .v-list-item--active {
+  color: white !important;
+}
+</style>
+
